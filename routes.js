aa764051b10928f3a3c3167e77308f9ba81e74d8
Kartono <kartono1102@gmail.com>
2020-10-04 21:26:52 +0700

    add function add in controller

--- routes.js
+++ routes.js
@@ -6,4 +6,5 @@ module.exports = function(app){
     app.route('/').get(myJson.index);
     app.route('/getmhs').get(myJson.getmhs);
     app.route('/getmhs/:id').get(myJson.getmhsByNpm);
+    app.route('/addmhs').post(myJson.addMhs);
 }
