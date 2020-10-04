d33af0c0a29b6cc0cf1af3133350526f3f77ec75
Kartono <kartono1102@gmail.com>
2020-10-04 21:25:50 +0700

    add function add in controller

--- controller.js
+++ controller.js
@@ -8,22 +8,43 @@ exports.index = function(req, res){
 };
 
 exports.getmhs = function(req, res){
-    connecti.query('SELECT * FROM data_mhs', function(error, rows, fields){
-        if(error){
-            connecti.log(error);
-        }else{
-            response.ok(rows, res);
+    connecti.query('SELECT * FROM tbl_mhs', 
+        function(error, rows, fields){
+            if(error){
+                connecti.log(error);
+            }else{
+                response.ok(rows, res);
+            }
         }
-    });
+    );
 };
 
 exports.getmhsByNpm = function(req, res){
     let npm = req.params.id;
-    connecti.query('SELECT * FROM data_mhs WHERE npm = ?', [npm], function(error, rows, fields){
-        if(error){
-            connecti.log(error);
-        }else{
-            response.ok(rows, res);
+    connecti.query('SELECT * FROM tbl_mhs WHERE npm = ?', [npm], 
+        function(error, rows, fields){
+            if(error){
+                connecti.log(error);
+            }else{
+                response.ok(rows, res);
+            }
         }
-    });
+    );
 };
+
+exports.addMhs = function(req, res){
+    let npm = req.body.npm;
+    let nama = req.body.nama;
+    let jk = req.body.jk;
+    let nohp = req.body.nohp;
+
+    connecti.query('INSERT INTO tbl_mhs VALUES(?,?,?,?)', [npm, nama, jk, nohp],
+        function(error, rows, fields){
+            if(error){
+                connecti.log(error);
+            }else{
+                response.ok('Add data mhs success !', res);
+            }
+        }
+    );
+}
